9b948bafa3e5fbcc0c00f605392ba3cfc0456209
JoJoBeBop <jori.blom@metropolia.fi>
2019-07-16 14:52:21 +0300

    Git init

--- part1/index.js
+++ part1/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan = require("morgan")
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
 
 const bodyParser = require('body-parser');
 
@@ -28,6 +32,15 @@ let persons = [
     },
 
 ];
+/*3.8*/
+morgan.token('content', function (request) {return JSON.stringify(request.body)});
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :content'));
+
+
+
+app.get('/persons', (request, response) => {
+    response.json(persons)
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -36,65 +49,68 @@ app.get('/info', (req, res) => {
     const date = new Date();
     const personsLength = persons.length;
 
-    res.send(
-        '<h4>This phonebook has '+ personsLength + ' numbers</h4>' +
-        '<h4>'+ date + '</h4>')
+    res.send(`<h4> This phone book has ${personsLength} numbers</h4><br><h4>${date}</h4>`)
+
 });
 
-const personsLength = () => {
-
-    const date = new Date();
-};
-
-
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0;
-    return maxId + 1
-};
-
-app.post('/persons', (request, response) => {
-    const body = request.body;
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const person = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    };
-
-    persons = persons.concat(person);
-
-    response.json(persons)
-});
-
-app.get('/persons', (request, response) => {
-    response.json(persons)
-});
-
+/*3.3*/
 app.get('/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     const person = persons.find(persons => persons.id === id);
     if (person) {
-        response.json(persons)
+        response.json(person)
     } else {
         response.status(404).end()
     }
 });
 
+/*3.4*/
 app.delete('/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     persons = persons.filter(person => person.id !== id);
 
     response.status(204).end()
 });
+
+const generateId = () => {
+    const randomNumber = () => {
+        const rand = Math.random() * (1000 - 100) + 1000
+        return Math.round(rand)
+    };
+    return randomNumber()
+};
+
+/*3.6*/
+app.post("/persons", (request, response) => {
+    const body = request.body;
+
+    const loopedNames = persons.map(per => per.name);
+
+    console.log(body);
+    if (!body.name || !body.number) {
+        return response.status(400).json({error: "no content or content missing"})
+    } else if (loopedNames.includes(body.name)) {
+        return response.status(400).json({error: "name has already been submitted"})
+
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    };
+
+    persons = persons.concat(person);
+    response.json(persons)
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+};
+
+
+app.use(unknownEndpoint);
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
